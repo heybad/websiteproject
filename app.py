16c2cfde7f5f98de9f155c1e8184805c868993c7
heybad <148739363+heybad@users.noreply.github.com>
2023-10-25 22:39:38 +0000

    updated

--- app.py
+++ app.py
@@ -8,4 +8,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000, debug=True)
+  # Use a production server (e.g., Gunicorn) and turn off debug mode
+  gunicorn_app = app
+  gunicorn_app.run(host='0.0.0.0', port=5000, debug=False)
+
