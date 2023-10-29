3a30557052429e44d0983de8fcbff4dcf2af5447
heybad <148739363+heybad@users.noreply.github.com>
2023-10-29 00:46:47 +0000

    add the loan data

--- app.py
+++ app.py
@@ -4,34 +4,34 @@ import pandas as pd
 app = Flask(__name__)
 
 # Read the CSV file
-df = pd.read_csv('Airlines_data.csv')
-
-# Convert the DataFrame to a list of dictionaries for easy rendering in HTML
+df = pd.read_csv('data/Airlines_data.csv')
+df1 = pd.read_csv('data/updated_loan.csv')
 data = df.to_dict(orient='records')
+datas = df1.to_dict(orient='records')
 
 @app.route('/')
 def index():
-  return render_template('main.html')
+    return render_template('main.html')
 
 @app.route('/home')
 def home():
-  return render_template('main.html') + render_template('home.html') 
+    return render_template('main.html') + render_template('home.html')
 
 @app.route('/portfolio')
 def portfolio():
-  return render_template('portfolio.html')
+    return render_template('portfolio.html')
 
 @app.route('/portfolio/airline')
 def airline():
     return render_template('airline.html', data=data)
-  
+
 @app.route('/portfolio/scooter')
 def scooter():
-    return render_template('scooter.html', data=data)
+    return render_template('scooter.html', data=datas)
 
 @app.route('/contact')
 def contact():
-  return render_template('contact.html')
+    return render_template('contact.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
